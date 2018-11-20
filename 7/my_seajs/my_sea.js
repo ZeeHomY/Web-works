163f26472e1f6e936b2c297343a4bd9ab365b9aa
yuezihao <yuezihao@joinquant.com>
2018-11-20 17:18:48 +0800

    update my_seajs

--- 7/my_seajs/my_sea.js
+++ 7/my_seajs/my_sea.js
@@ -3,7 +3,12 @@ const sea = {
     $.ajax({                   // 用$.ajax();模拟seajs的异步获取文件方式
       url: path,
       success (str) {
-        alert();
+
+        function define (fn) {     // 在eval使代码生效前声明define
+          fn();
+        }
+
+        eval(str);
       },
       error () {
         alert('失败');
